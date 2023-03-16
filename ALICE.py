e4e928c685de16826a33bf80eb0f4f2c283af210
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-16 03:34:40 -0500

    Update ALICE.py
    reads from SK.json

--- ALICE.py
+++ ALICE.py
@@ -1,9 +1,11 @@
+# ALICE V2.0 (Stable)
+# Character count: 2439
+
 import json
 import nltk
 
-# Download the 'punkt' resource
+# Download the 'punkt' resource (suppress download messages)
 nltk.download('punkt', quiet=True)
-
 
 # Set the NLTK data path
 nltk.data.path.append(r'C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2800.0_x64__qbz5n2kfra8p0\nltk_data')
@@ -18,8 +20,9 @@ def load_qa_pairs(file_path):
 
 # Function to find a response for a given question using the loaded Q&A pairs
 def find_response(qa_pairs, question):
+    question_tokens = word_tokenize(question.lower())
     for pair in qa_pairs:
-        if pair["question"].lower() == question.lower():
+        if set(word_tokenize(pair["question"].lower())) == set(question_tokens):
             return pair["answer"]
     return None
 
@@ -36,29 +39,4 @@ def process_input(user_input, qa_pairs):
         farewells = ['bye', 'goodbye', 'see you']
 
         if any(token in greetings for token in tokens):
-            response = "Hello! How can I help you?"
-        elif any(token in farewells for token in tokens):
-            response = "Goodbye! Have a great day!"
-        else:
-            response = "I'm not sure how to respond. Please try a different query."
-
-    return response
-
-# Update the main function to load Q&A pairs and pass them to the process_input function
-def main():
-    print("Welcome to ALICE V1.1! (type 'quit' to exit)")
-
-    # Load Q&A pairs from the JSON file
-    qa_pairs = load_qa_pairs('SK.json')
-
-    while True:
-        user_input = input("User: ")
-
-        if user_input.lower() == 'quit':
-            break
-
-        response = process_input(user_input, qa_pairs)
-        print("ALICE:", response)
-
-if __name__ == "__main__":
-    main()
+            response = "Hello! How can I help
