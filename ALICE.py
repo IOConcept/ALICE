99b111f8012df9cf6330989c22d831f406b7d0a5
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-16 03:30:42 -0500

    Update ALICE.py

--- ALICE.py
+++ ALICE.py
@@ -1,33 +1,55 @@
+import json
 import nltk
 
 # Download the 'punkt' resource
-nltk.download('punkt')
+nltk.download('punkt', quiet=True)
+
 
 # Set the NLTK data path
 nltk.data.path.append(r'C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2800.0_x64__qbz5n2kfra8p0\nltk_data')
 
 from nltk.tokenize import word_tokenize
 
-# Define a function to process user input and generate a response
-def process_input(user_input):
-    # Tokenize the user input
-    tokens = word_tokenize(user_input.lower())
+# Function to load Q&A pairs from the JSON file
+def load_qa_pairs(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        qa_pairs = json.load(file)
+    return qa_pairs
 
-    # Define keywords and corresponding responses
-    greetings = ['hello', 'hi', 'hey', 'greetings']
-    farewells = ['bye', 'goodbye', 'see you']
+# Function to find a response for a given question using the loaded Q&A pairs
+def find_response(qa_pairs, question):
+    for pair in qa_pairs:
+        if pair["question"].lower() == question.lower():
+            return pair["answer"]
+    return None
 
-    # Check if any keyword is present in the tokenized input
-    if any(token in greetings for token in tokens):
-        return "Hello! How can I help you?"
-    elif any(token in farewells for token in tokens):
-        return "Goodbye! Have a great day!"
-    else:
-        return "I'm not sure how to respond. Please try a different query."
+# Update the process_input function to use the Q&A pairs
+def process_input(user_input, qa_pairs):
+    # Find a response from the Q&A pairs
+    response = find_response(qa_pairs, user_input)
 
-# Main function to run ALICE
+    # If no response is found, use the original keyword-based approach
+    if not response:
+        tokens = word_tokenize(user_input.lower())
+
+        greetings = ['hello', 'hi', 'hey', 'greetings']
+        farewells = ['bye', 'goodbye', 'see you']
+
+        if any(token in greetings for token in tokens):
+            response = "Hello! How can I help you?"
+        elif any(token in farewells for token in tokens):
+            response = "Goodbye! Have a great day!"
+        else:
+            response = "I'm not sure how to respond. Please try a different query."
+
+    return response
+
+# Update the main function to load Q&A pairs and pass them to the process_input function
 def main():
-    print("Welcome to ALICE! (type 'quit' to exit)")
+    print("Welcome to ALICE V1.1! (type 'quit' to exit)")
+
+    # Load Q&A pairs from the JSON file
+    qa_pairs = load_qa_pairs('SK.json')
 
     while True:
         user_input = input("User: ")
@@ -35,7 +57,7 @@ def main():
         if user_input.lower() == 'quit':
             break
 
-        response = process_input(user_input)
+        response = process_input(user_input, qa_pairs)
         print("ALICE:", response)
 
 if __name__ == "__main__":
