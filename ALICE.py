3c84734b94b719ba1cb6cfce47b8e104779e57cd
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-17 02:51:54 -0500

    Update ALICE.py
    it is woring!

--- ALICE.py
+++ ALICE.py
@@ -1,37 +1,45 @@
-# ALICE V2.0 (Stable)
-# Character count: 2439
-
+# ALICE V2.4 (Approximate character count: 3800)
 import json
 import nltk
-
-# Download the 'punkt' resource (suppress download messages)
-nltk.download('punkt', quiet=True)
-
-# Set the NLTK data path
-nltk.data.path.append(r'C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2800.0_x64__qbz5n2kfra8p0\nltk_data')
-
 from nltk.tokenize import word_tokenize
 
-# Function to load Q&A pairs from the JSON file
-def load_qa_pairs(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        qa_pairs = json.load(file)
-    return qa_pairs
+nltk.download('punkt', quiet=True)
 
-# Function to find a response for a given question using the loaded Q&A pairs
-def find_response(qa_pairs, question):
-    question_tokens = word_tokenize(question.lower())
+def find_response(question, qa_pairs):
+    tokens = set(word_tokenize(question.lower()))
+    best_match = None
+    max_common_tokens = 0
+    threshold = 1
+
     for pair in qa_pairs:
-        if set(word_tokenize(pair["question"].lower())) == set(question_tokens):
-            return pair["answer"]
-    return None
+        pair_tokens = set(word_tokenize(pair["question"].lower()))
+        common_tokens = len(tokens.intersection(pair_tokens))
 
-# Update the process_input function to use the Q&A pairs
-def process_input(user_input, qa_pairs):
-    # Find a response from the Q&A pairs
-    response = find_response(qa_pairs, user_input)
+        if common_tokens > max_common_tokens:
+            max_common_tokens = common_tokens
+            best_match = pair["answer"]
 
-    # If no response is found, use the original keyword-based approach
+    if max_common_tokens < threshold:
+        return None
+
+    return best_match
+
+def load_qa_pairs(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_to_ac(file_path, question, explanation):
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    data.append({"question": question, "answer": explanation})
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def process_input(user_input, sk_pairs, ac_pairs, ac_file_path):
+    response = find_response(user_input, sk_pairs + ac_pairs)
+
     if not response:
         tokens = word_tokenize(user_input.lower())
 
@@ -39,4 +47,36 @@ def process_input(user_input, qa_pairs):
         farewells = ['bye', 'goodbye', 'see you']
 
         if any(token in greetings for token in tokens):
-            response = "Hello! How can I help
+            response = "Hello! How can I help you?"
+        elif any(token in farewells for token in tokens):
+            response = "Goodbye! Have a great day!"
+        else:
+            response = None
+
+    if not response:
+        print("ALICE: I do not understand {}, please explain further.".format(user_input))
+        user_explanation = input("User: ")
+        save_to_ac(ac_file_path, user_input, user_explanation)
+        response = "Thank you for explaining. I have added this information to my knowledge."
+
+    return response
+
+def main():
+    sk_file_path = "SK.json"
+    ac_file_path = "AC.json"
+    sk_pairs = load_qa_pairs(sk_file_path)
+    ac_pairs = load_qa_pairs(ac_file_path)
+
+    print("Welcome to ALICE V2.4! (type 'quit' to exit)")
+
+    while True:
+        user_input = input("User: ")
+
+        if user_input.lower() == 'quit':
+            break
+
+        response = process_input(user_input, sk_pairs, ac_pairs, ac_file_path)
+        print("ALICE:", response)
+
+if __name__ == "__main__":
+    main()
